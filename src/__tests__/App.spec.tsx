0fbab0416a03d251efb6456c0c50c4c435933484
throwawayer <zurdig@gmail.com>
2021-02-08 16:16:45 +0200

    fix/DCVD-1: various fixes - add back webpack-dev-server - adjust App layout - fix unit test for App layout

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,19 +1,12 @@
 import '@testing-library/jest-dom';
-import {
-  waitFor, render, screen,
-} from '@testing-library/react';
+import { waitFor, render, screen } from '@testing-library/react';
 
 import React from 'react';
 
 import AppContainer from 'containers/AppContainer';
 
-const firstParagraph = `The following application gets a list of documents from a remote
-server and use that remote server to check the validity of those documents.`;
-
-const secondParagraph = `The last column in the table contains a button that allows a
-user to validate a document. The result of validation is shown via status circles,
-which show the progress in standard way: gray stands for idle, rotating for pending,
-green for successful validation, red for the failed one.`;
+const firstParagraph = `The following application gets a list of documents from a remote server and use that remote server to check the validity of those documents.`;
+const secondParagraph = `The last column in the table contains a button that allows a user to validate a document. The result of validation is shown via status circles, which show the progress in standard way:`;
 
 describe('AppContainer', () => {
   it('should render correctly', async () => {
@@ -29,5 +22,9 @@ describe('AppContainer', () => {
 
     expect(screen.getByText(firstParagraph)).toBeInTheDocument();
     expect(screen.getByText(secondParagraph)).toBeInTheDocument();
+    expect(screen.getByText('gray')).toHaveClass('stressed-idle');
+    expect(screen.getByText('rotating')).toHaveClass('stressed-pending');
+    expect(screen.getByText('green')).toHaveClass('stressed-success');
+    expect(screen.getByText('red')).toHaveClass('stressed-error');
   });
 });
