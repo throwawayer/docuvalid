7d0883aabc53d53abb3497e099be8a13984bc7c3
throwawayer <zurdig@gmail.com>
2021-02-08 18:32:29 +0200

    fix/DCVD-2: fix linting

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import '@testing-library/jest-dom';
 import { waitFor, render, screen } from '@testing-library/react';
 
@@ -5,8 +6,10 @@ import React from 'react';
 
 import AppContainer from 'containers/AppContainer';
 
-const firstParagraph = `The following application gets a list of documents from a remote server and use that remote server to check the validity of those documents.`;
-const secondParagraph = `The last column in the table contains a button that allows a user to validate a document. The result of validation is shown via status circles, which show the progress in standard way:`;
+const firstParagraph =
+  'The following application gets a list of documents from a remote server and use that remote server to check the validity of those documents.';
+const secondParagraph =
+  'The last column in the table contains a button that allows a user to validate a document. The result of validation is shown via status circles, which show the progress in standard way:';
 
 describe('AppContainer', () => {
   it('should render correctly', async () => {
